fc1a6506fec90b93c9c5b3e3f5a5e051f5823371
Oleksii Fedorov <waterlink000@gmail.com>
2017-02-08 17:52:26 +0100

    Initial commit: create a golden master test for function main and extract SongWriter class

---
+++ test/MainTest.java
@@ -0,0 +1,67 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest extends PrintStream {
+
+    private String output = "";
+
+    MainTest() {
+        super(System.err);
+        System.setOut(this);
+    }
+
+    @Override
+    public void println(String s) {
+        output += s;
+    }
+
+    @Test
+    void main() {
+        Main.main(new String[]{});
+
+        assertEquals("There was an old lady who swallowed a fly.\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+                "\n" +
+                "There was an old lady who swallowed a spider;\n" +
+                "That wriggled and wiggled and tickled inside her.\n" +
+                "She swallowed the spider to catch the fly;\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+                "\n" +
+                "There was an old lady who swallowed a bird;\n" +
+                "How absurd to swallow a bird.\n" +
+                "She swallowed the bird to catch the spider,\n" +
+                "She swallowed the spider to catch the fly;\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+                "\n" +
+                "There was an old lady who swallowed a cat;\n" +
+                "Fancy that to swallow a cat!\n" +
+                "She swallowed the cat to catch the bird,\n" +
+                "She swallowed the bird to catch the spider,\n" +
+                "She swallowed the spider to catch the fly;\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+                "\n" +
+                "There was an old lady who swallowed a dog;\n" +
+                "What a hog, to swallow a dog!\n" +
+                "She swallowed the dog to catch the cat,\n" +
+                "She swallowed the cat to catch the bird,\n" +
+                "She swallowed the bird to catch the spider,\n" +
+                "She swallowed the spider to catch the fly;\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+                "\n" +
+                "There was an old lady who swallowed a cow;\n" +
+                "I don't know how she swallowed a cow!\n" +
+                "She swallowed the cow to catch the dog,\n" +
+                "She swallowed the dog to catch the cat,\n" +
+                "She swallowed the cat to catch the bird,\n" +
+                "She swallowed the bird to catch the spider,\n" +
+                "She swallowed the spider to catch the fly;\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+                "\n" +
+                "There was an old lady who swallowed a horse...\n" +
+                "...She's dead, of course!", output);
+    }
+
+}
